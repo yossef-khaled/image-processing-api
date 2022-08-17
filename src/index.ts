880b2d5e0ebc29c9fe0b739ba8c992488b583b6c
yossef-khaled <yossef.k.y333@gmail.com>
2022-08-17 02:04:33 +0200

    Set up the listening port

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// import express 
+import express  from "express";
+
+// import routes
+import imagesRoutes from "./routes/images";
+
+
+const port = 3000;
+const app = express();
+
+app.use('/images', imagesRoutes);
+
+app.listen(port, () => {
+    console.log(`🚀🚀 Image processing API Server started on port ${port}...`)
+})
