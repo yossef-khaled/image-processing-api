d7f52399cdfd7a856a33676ff5bd01c3a06d6254
yossef-khaled <yossef.k.y333@gmail.com>
2022-08-18 16:36:56 +0200

    Add a function to check whether a nimage exists or not

---
+++ src/utilities/getImage.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+
+function getImage(path: string): Promise<Buffer> {
+
+    return new Promise(async (resolve, reject) => {
+        
+        await fs.readFile(path, (err: NodeJS.ErrnoException | null, resizedImage: Buffer) => {
+            if(err) {
+                reject(err);
+            }
+
+            resolve(resizedImage);
+        });
+    })
+}
+
+export default getImage;
