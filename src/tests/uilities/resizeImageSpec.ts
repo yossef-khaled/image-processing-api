b47be3ae56d06e5edc445a1bbe8c2b81178b4fc4
yossef-khaled <yossef.k.y333@gmail.com>
2022-08-20 13:47:26 +0200

    Add a suite for the resizeImage function

---
+++ src/tests/uilities/resizeImageSpec.ts
@@ -0,0 +1,31 @@
+import resizeImage from "../../utilities/resizeImage";
+import fs from 'fs';
+
+const testImage: string = 'santamonica'
+const testImagePath: string = `src/assets/originalImages/${testImage}.jpg`;
+const width: number = 380;
+const height: number = 500;
+const destinationImagePath: string = `src/assets/editedImages/${testImage}_${width}x${height}.jpg`; 
+
+describe('resizeImage function', () => {
+    it('Should not return any thing with a file created at the destination image path', async () => {
+        resizeImage(testImagePath, destinationImagePath, width, height)
+        .then(() => {
+            expect(fs.existsSync(destinationImagePath)).toBe(true);
+        })
+    })
+
+    it('Should not throw an error', async () => {
+        resizeImage(testImagePath, destinationImagePath, width, height)
+        .then((value) => {
+            expect(value).not.toThrow();
+        })
+    })
+
+    it('Should throw an error', async () => {
+        resizeImage('random path', destinationImagePath, width, height)
+        .then((value) => {
+            expect(value).toThrow();
+        })
+    })
+})
